7416a8ff4f9e640ecae322d62498bec18e552379
kevinaragn <kevinmartinaragon@gmail.com>
2022-10-12 22:21:35 -0300

    formulario aragon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { PadreComponent } from './components/padre/padre.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    TablaComponent,
+    PadreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
